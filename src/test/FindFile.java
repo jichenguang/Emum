1e3a934a031adcce888c309919036e69a27816c1
jichenguang <jichenguang@700bike.com>
2016-02-26 16:32:30 +0800

    something test script.

---
+++ src/test/FindFile.java
@@ -0,0 +1,12 @@
+package test;
+
+public class FindFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String config = System.getProperty("user.dir")
+				+ "/config.properties";
+		System.out.println(config);
+	}
+
+}
